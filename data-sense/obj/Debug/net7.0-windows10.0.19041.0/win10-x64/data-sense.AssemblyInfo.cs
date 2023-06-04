4b7258e34014b2b39bd4b5845b0c342f26f9b9ee
Qusai Sabri <qusai.sabri19@gmail.com>
2023-06-03 22:15:59 -0500

    Layout test

--- data-sense/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/data-sense.AssemblyInfo.cs
+++ data-sense/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/data-sense.AssemblyInfo.cs
@@ -11,14 +11,10 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "0ae9c9c4-3b3f-47c2-a044-2996cd0d1124")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "data-sense")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("data-sense")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("data-sense")]
 [assembly: System.Reflection.AssemblyTitleAttribute("data-sense")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
